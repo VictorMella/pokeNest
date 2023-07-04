f7fc65fb718f977f5e890a69b8a7535fb198f3a6
Victor <vmellaq@mcad43x2f.corporacionbci.cl>
2023-07-04 10:40:42 -0400

    update value

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 export const EnvConfiguration = () => ({
   envitonment: process.env.NODE_ENV || 'dev',
   mongodb: process.env.MONGODB,
-  port: parseInt(process.env.PORT) || 3001,
-  defaultLimit: parseInt(process.env.DEFAULT_LIMIT) || 7,
+  port: +process.env.PORT || 3001,
+  defaultLimit: +process.env.DEFAULT_LIMIT || 7,
 });
